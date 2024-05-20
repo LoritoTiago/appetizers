8727f4d241562b89ccf6c53820ea8ad9f56a99be
Lorito Tiago <58330997+LoritoTiago@users.noreply.github.com>
2024-05-20 11:10:57 +0100

    Initial Commit

---
+++ appetizers/appetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  appetizersApp.swift
+//  appetizers
+//
+//  Created by Lorito Mbuta on 20/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct appetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
